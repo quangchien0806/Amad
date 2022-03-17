72284915ebdf373d4ff1fd939edb7a8db52c15ee
Your Name <quangchien220399@gmail.com>
2022-03-17 16:52:07 +0700

    abc

--- main.js
+++ main.js
@@ -12,15 +12,15 @@ window.addEventListener("scroll", () => {
   }
 });
 
-scrollUp.addEventListener("click", scrollToTop);
+// scrollUp.addEventListener("click", scrollToTop);
 
-function scrollToTop() {
-  var position = document.body.scrollTop || document.documentElement.scrollTop;
-  if (position) {
-    window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
-    scrollAnimation = setTimeout("scrollToTop()", 30);
-  } else clearTimeout(scrollAnimation);
-}
+// function scrollToTop() {
+//   var position = document.body.scrollTop || document.documentElement.scrollTop;
+//   if (position) {
+//     window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
+//     scrollAnimation = setTimeout("scrollToTop()", 30);
+//   } else clearTimeout(scrollAnimation);
+// }
 
 // Click menu
 mobi_togglers.addEventListener("click", () => {
